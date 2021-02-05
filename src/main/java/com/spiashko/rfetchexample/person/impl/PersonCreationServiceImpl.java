00667caa140f0310452b893c6b594986c54b56c0
Siarhei Piashko <peshko.sergey@gmail.com>
2021-02-05 18:44:47 +0300

    add transactional listener setup

--- src/main/java/com/spiashko/rfetchexample/person/impl/PersonCreationServiceImpl.java
+++ src/main/java/com/spiashko/rfetchexample/person/impl/PersonCreationServiceImpl.java
@@ -2,7 +2,9 @@ package com.spiashko.rfetchexample.person.impl;
 
 import com.spiashko.rfetchexample.person.Person;
 import com.spiashko.rfetchexample.person.PersonCreationService;
+import com.spiashko.rfetchexample.person.appevent.PersonCreatedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,10 +12,18 @@ import org.springframework.stereotype.Service;
 class PersonCreationServiceImpl implements PersonCreationService {
 
     private final PersonRepository repository;
+    private final ApplicationEventPublisher publisher;
 
     @Override
     public Person create(Person entityToCreate) {
         Person entity = repository.save(entityToCreate);
+
+        publisher.publishEvent(
+                PersonCreatedEvent.builder()
+                        .person(entity)
+                        .build()
+        );
+
         return entity;
     }
 
